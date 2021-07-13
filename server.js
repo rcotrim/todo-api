1e4262e245c67416ba6ee7dae8bfd25fbd5fe006
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-07-13 15:45:15 -0300

    GET API /todos

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ var app = express();
 //var PORT = 3000;
 //Para usar heroku:
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Almoço com Carlos',
+    completed: false
+},{
+    id: 2,
+    description: 'Ir ao super',
+    completed: false
+}, {
+    id: 3,
+    description: 'Colocar gasolina',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('TODO API root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos', function (req, res) {
+
 });
 
 app.listen(PORT, function() {
