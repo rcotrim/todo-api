e1ed740b8a459f689a4e913e039102d68b035d0c
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-07-13 16:42:32 -0300

    POST /todos - crair novo todo

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var express = require('express');
+//var bodyParser = require('body-parser');
+
 var app = express();
 //var PORT = 3000;
 //Para usar heroku:
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Almoço com Carlos',
-    completed: false
-},{
-    id: 2,
-    description: 'Ir ao super',
-    completed: false
-}, {
-    id: 3,
-    description: 'Colocar gasolina',
-    completed: true
-}];
+var todos = [];
+var todoNextId =1;
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send('TODO API root');
@@ -42,6 +35,21 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
 });
+
+//POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    //console.log('description: ' + body.description);
+
+    // add id field
+    body.id = todoNextId++;
+
+    // push body into array
+    todos.push(body);
+
+    res.json(body);
+});
+
 
 app.listen(PORT, function() {
     console.log('Express escutando na porta ' + PORT + '!');
