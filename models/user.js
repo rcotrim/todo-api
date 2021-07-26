93cdec64c4c2a0cf3593dc1d7dacd06c306c58d1
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-07-26 16:53:02 -0300

    Convertendo e-Mail apra lower case usando hooks

--- models/user.js
+++ models/user.js
@@ -16,7 +16,15 @@ module.exports = function (sequelize) {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [7,100]
+                len: [7, 100]
+            }
+        }
+    }, {
+        hooks: {
+            beforeValidate: function (user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase()
+                }
             }
         }
     });
